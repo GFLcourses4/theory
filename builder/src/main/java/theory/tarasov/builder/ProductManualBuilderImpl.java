4de83faf774ea5e7d8d4811ace9abd1f58ed9b43
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-05-13 20:48:45 +0300

    implemented ProductManualBuilder interface

---
+++ builder/src/main/java/theory/tarasov/builder/ProductManualBuilderImpl.java
@@ -0,0 +1,42 @@
+package theory.tarasov.builder;
+
+public class ProductManualBuilderImpl implements ProductManualBuilder {
+    private String manufacturer;
+    private final ProductBuilder productBuilder;
+
+    public ProductManualBuilderImpl(ProductBuilder productBuilder) {
+        this.productBuilder = productBuilder;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        productBuilder.setDescription(description);
+    }
+
+    @Override
+    public void setName(String name) {
+        productBuilder.setName(name);
+    }
+
+    @Override
+    public void setPrice(long price) {
+        productBuilder.setPrice(price);
+    }
+
+    @Override
+    public void setQuantity(int quantity) {
+        productBuilder.setQuantity(quantity);
+    }
+
+    @Override
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public Product getProduct() {
+        var product = productBuilder.getProduct();
+        product.setManufacturer(manufacturer);
+        return product;
+    }
+}
