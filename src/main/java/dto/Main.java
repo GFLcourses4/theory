48e0090efd5676c645f3ab5c2cd268be41499995
Katya <ksemeukina@gmail.com>
2023-05-14 23:31:37 +0200

    Pattern DTO

---
+++ src/main/java/dto/Main.java
@@ -0,0 +1,11 @@
+package dto;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("ksemeikina", "psRt23_@", 1499070300000L);
+        System.out.println(user);
+        UserMapper userMapper = new UserMapper();
+        UserDTO userDTO = userMapper.UserToUserDTO(user);
+        System.out.println(userDTO);
+    }
+}
