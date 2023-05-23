835e1f97fa1dbef357f0557099531d3e10ba8780
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-05-23 18:58:21 +0300

    created App

---
+++ factory/src/main/java/theory/tarasov/App.java
@@ -0,0 +1,19 @@
+package theory.tarasov;
+
+import theory.tarasov.calc.Calc;
+import theory.tarasov.calc.EngineeringCalc;
+import theory.tarasov.calc.EngineeringCalcImpl;
+import theory.tarasov.factory.CachedServiceFactoryProvider;
+import theory.tarasov.factory.DIFactory;
+
+public class App {
+    public static void main(String[] args) {
+        DIFactory factory = CachedServiceFactoryProvider.getFactory();
+        EngineeringCalc engineeringCalc = factory.createInstance(EngineeringCalc.class);
+        Calc calc = factory.createInstance(Calc.class);
+
+        if(engineeringCalc instanceof EngineeringCalcImpl impl) {
+            System.out.println(impl.getCalc() == calc);
+        }
+    }
+}
