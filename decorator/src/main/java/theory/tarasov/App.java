d715da9af47cb9da8fb171d7412b560268e3fea6
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-06-30 17:02:19 +0300

    added examples

--- decorator/src/main/java/theory/tarasov/App.java
+++ decorator/src/main/java/theory/tarasov/App.java
@@ -4,14 +4,18 @@ import theory.tarasov.decorator.BlurFilterDecorator;
 import theory.tarasov.decorator.BrightnessFilterDecorator;
 import theory.tarasov.decorator.GrayscaleFilterDecorator;
 import theory.tarasov.decorator.InitialImageFilter;
+import theory.tarasov.inheritancedecorator.BaseMessage;
+import theory.tarasov.inheritancedecorator.FormattingDecorator;
+import theory.tarasov.inheritancedecorator.SignatureDecorator;
 
 public class App {
     public static void main(String[] args) {
-        var decoratedFilter = new GrayscaleFilterDecorator(
-                new BlurFilterDecorator(
-                        new BrightnessFilterDecorator(
-                                new InitialImageFilter()))
-        );
-        decoratedFilter.applyFilter("image.jpg");
+        new GrayscaleFilterDecorator(new BlurFilterDecorator(new BrightnessFilterDecorator(new InitialImageFilter()))).applyFilter("image.jpg");
+        System.out.println();
+        new BlurFilterDecorator(new BrightnessFilterDecorator(new GrayscaleFilterDecorator(new InitialImageFilter()))).applyFilter("another image.jpg");
+        System.out.println();
+        new SignatureDecorator(new FormattingDecorator(new BaseMessage("some message")), "best regards").send();
+        System.out.println();
+        new FormattingDecorator(new SignatureDecorator(new BaseMessage("some other message"), "best regards")).send();
     }
 }
